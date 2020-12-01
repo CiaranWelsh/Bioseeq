e618cbb0c48a464283922b5e32e1c49199acd766
Ciaran Welsh <cwelsh2@uw.edu>
2020-12-01 15:22:38 +0000

    first commit. Starting repository

---
+++ src/Download.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Ciaran on 01/12/2020.
+//
+
+#include "Download.h"
+
+namespace bioseeq {
+
+}
---
+++ src/Download.h
@@ -0,0 +1,14 @@
+//
+// Created by Ciaran on 01/12/2020.
+//
+
+#ifndef BIOSEEK_DOWNLOAD_H
+#define BIOSEEK_DOWNLOAD_H
+
+namespace bioseeq {
+    class Download {
+
+    };
+}
+
+#endif //BIOSEEK_DOWNLOAD_H
